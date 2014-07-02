b9fb47aba52c77b335dcc69ee53c89571f6595ec
Gerold Meisinger <gerold.meisinger@gmail.com>
2014-07-02 22:59:41 +0100

    removed usage of base64.js Relates to #133

--- plugin/chrome/content/common.js
+++ plugin/chrome/content/common.js
@@ -210,9 +210,8 @@ var Pers_util = {
 	// here, and re-encode the signature in base64.
 	// see firefox-v2/xp_src/moz_crypto.cpp for details of header format
 	add_der_signature_header: function(sig_base64) {
-
-		var base_str = Pers_Base64.decode(sig_base64);
-		var der_header_md5 = [ "0x30", "0x81", "0xbf", "0x30", "0x0d", "0x06",
+		var base_str = window.atob(sig_base64);
+		var der_header_md5 = ["0x30", "0x81", "0xbf", "0x30", "0x0d", "0x06",
 							"0x09", "0x2a", "0x86", "0x48", "0x86", "0xf7",
 							"0x0d", "0x01", "0x01", "0x04", "0x05", "0x00",
 							"0x03", "0x81", "0xad", "0x00"];
@@ -220,7 +219,7 @@ var Pers_util = {
 		for (var i = 0; i < der_header_md5.length; i++) {
 			header_str += String.fromCharCode(parseInt(der_header_md5[i],16));
 		}
-		return Pers_Base64.encode(header_str + base_str) ;
+		return window.btoa(header_str + base_str);
 	},
 
 
